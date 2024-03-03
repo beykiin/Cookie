109c71c807777aaa939cac90cc72509930ade640
Yasin Beken <yasinbeken16@gmail.com>
2024-03-03 13:25:47 +0300

    First Commit

---
+++ cookie.js
@@ -0,0 +1,11 @@
+
+const btn = document.querySelector("#btn")
+const text=document.querySelector("#text")
+
+
+btn.onclick=()=>{
+    document.cookie="username=bekenyasin;"
+}
+// btn.onclick=()=>{
+//     document.cookie=`text= ${text.value}`
+// }
